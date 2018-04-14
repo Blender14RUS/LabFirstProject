aee9ae3652fd3606c19a52caafa85cbab44496f6
Vlad <skrebcovvlad@gmail.com>
2018-04-15 01:18:24 +0300

    Revert "Revert "IU-2017.2.5 <Vlad@Vlad-PC Merge branch 'master'""
    This reverts commit fae9e205b3e469dfd3d207726a9f3b65f790cd8d.

--- src/main/java/com/epam/lab/library/config/LibApplication.java
+++ src/main/java/com/epam/lab/library/config/LibApplication.java
@@ -1,4 +1,4 @@
-package com.epam.lab.library.web.config;
+package com.epam.lab.library.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
