16133729ac2b3115eb207391abe31c0ac4c4bd78
ksil29 <ksil@yandex.ru>
2018-04-15 17:22:12 +0300

    add test for getAllOrderByStatus add equals and hashCode for Order

--- src/main/java/com/epam/lab/library/domain/Order.java
+++ src/main/java/com/epam/lab/library/domain/Order.java
@@ -1,5 +1,7 @@
 package com.epam.lab.library.domain;
 
+import java.util.Objects;
+
 public class Order {
     private Long id;
     private Long userId;
@@ -55,5 +57,22 @@ public class Order {
 
     public void setStatus(String status) {
         this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return Objects.equals(id, order.id) &&
+                Objects.equals(userId, order.userId) &&
+                Objects.equals(bookId, order.bookId) &&
+                Objects.equals(location, order.location) &&
+                Objects.equals(status, order.status);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, userId, bookId, location, status);
     }
 }
