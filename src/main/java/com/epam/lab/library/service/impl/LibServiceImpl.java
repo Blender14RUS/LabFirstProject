68d2829fba7f1180b1bdb71130759517d829f2d2
Igor <bender14rus@gmail.com>
2018-04-15 20:26:14 +0300

    added log4j

--- src/main/java/com/epam/lab/library/service/impl/LibServiceImpl.java
+++ src/main/java/com/epam/lab/library/service/impl/LibServiceImpl.java
@@ -3,12 +3,17 @@ package com.epam.lab.library.service.impl;
 import com.epam.lab.library.dao.LibDao;
 import com.epam.lab.library.domain.Book;
 import com.epam.lab.library.service.LibService;
+import com.epam.lab.library.web.controller.BookController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LibServiceImpl implements LibService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(LibServiceImpl.class);
 
     private LibDao libDao;
 
@@ -23,6 +28,7 @@ public class LibServiceImpl implements LibService {
             book = libDao.addBook(book);
         }
         catch(DataAccessException e) {
+            LOG.error("Book not add to database -- " + e);
             book.setId(null);
         }
         return book;
--- src/main/java/com/epam/lab/library/web/controller/BookController.java
+++ src/main/java/com/epam/lab/library/web/controller/BookController.java
@@ -1,7 +1,10 @@
 package com.epam.lab.library.web.controller;
 
+import com.epam.lab.library.config.LibApplication;
 import com.epam.lab.library.domain.Book;
 import com.epam.lab.library.service.LibService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BookController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(BookController.class);
 
     @Autowired
     private LibService libService;
@@ -24,6 +29,7 @@ public class BookController {
     public String createBook(@RequestParam("title") String title, @RequestParam("year") int year,
                              @RequestParam("available") int available) {
         Book book = libService.addBook(new Book( title, year, available));
+        LOG.info("Add book: " + book.toString());
         return book.getId() == null ? "bookCreationFailure" : "redirect:/index";
     }
 
