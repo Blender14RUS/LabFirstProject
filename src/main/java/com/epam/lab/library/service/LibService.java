5213e768d517cee0f49ea5bf749d37d337a7b9b8
MatveyD <matv1995@gmail.com>
2018-04-16 15:29:21 +0300

    Admin page v0.7 add enum add simple javadoc

--- src/main/java/com/epam/lab/library/service/LibService.java
+++ src/main/java/com/epam/lab/library/service/LibService.java
@@ -8,39 +8,40 @@ import java.util.List;
 public interface LibService {
 
     /**
+     * Takes user data from database by user id
      *
-     * @param id
-     * @return
+     * @param id It's user id value in database
+     * @return an instances of User filled with data from the database
      */
     User getUser(Long id);
 
     /**
      * Creates List of all users from database
      *
-     * @return
+     * @return List of User instances filled with data from the database
      */
     List<User> getAllUsers();
 
     /**
      * Creates new user through web form
      *
-     * @param user
-     * @return
+     * @param user - an instances of User filled with data must be recorded in the database
+     * @return Count of changes rows in database
      */
     int createUser(User user);
 
     /**
-     * Method removes user from databese
+     * Method removes user from database
      *
-     * @param id
+     * @param id It's id of user which must be deleted from the database
      */
     void deleteUserById(Long id);
 
     /**
      * Changes user access level (READER / LIBRARIAN)
      *
-     * @param id
-     * @param accessLevel
+     * @param id It's id of user which access level must be changed
+     * @param accessLevel current value of user access level
      */
     void updateUserAccessLevel(Long id, AccessLevel accessLevel);
 
