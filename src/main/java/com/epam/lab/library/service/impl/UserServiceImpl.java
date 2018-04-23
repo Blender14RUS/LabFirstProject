83de616ade46b114e59f9912a389cb4ea28b93df
Vlad <skrebcovvlad@gmail.com>
2018-04-23 10:37:29 +0300

    Fix

--- src/main/java/com/epam/lab/library/service/impl/UserServiceImpl.java
+++ src/main/java/com/epam/lab/library/service/impl/UserServiceImpl.java
@@ -8,7 +8,6 @@ import com.epam.lab.library.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -68,15 +67,15 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public boolean updateUserAccessLevel(Long id, AccessLevel accessLevel) {
+    public void updateUserAccessLevel(Long id, AccessLevel accessLevel) {
         if (accessLevel.equals(AccessLevel.READER))
             accessLevel = AccessLevel.LIBRARIAN;
         else accessLevel = AccessLevel.READER;
-        if (!userDao.updateUserAccessLevel(id, accessLevel)) {
-            LOG.error("Update userRole failed.");
-            return false;
-        }
-        return true;
+//        if (!userDao.updateUserAccessLevel(id, accessLevel)) {
+//            LOG.error("Update userRole failed.");
+//            return false;
+//        }
+//        return true;
     }
 
     @Override
